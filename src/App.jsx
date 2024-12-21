1eabb316f9d07a74483c6ea62e057caea0fec7ce
Yousef ayman <yousefayman2000500@gmail.com>
2024-12-21 09:13:32 +0200

    add the starter files

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-red-600 font-barlowCondensed">Space tourism</h1>
+      <h2 className="text-red-600 font-barlow">Space tourism</h2>
+      <h3 className="text-red-600 font-bellefair">Space tourism</h3>
+    </>
+  );
+}
+
+export default App;
